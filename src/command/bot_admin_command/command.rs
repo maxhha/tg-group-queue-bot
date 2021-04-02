664884d346bf9fa4ccd4682d30e6b3a885007fa3
migregal <devgregorymironov@gmail.com>
2021-04-03 02:07:50 +0300

    Bot admin commands added

---
+++ src/command/bot_admin_command/command.rs
@@ -0,0 +1,64 @@
+use teloxide::prelude::*;
+
+use std::error::Error;
+
+type Cx = UpdateWithCx<AutoSend<Bot>, Message>;
+type Res = Result<(), Box<dyn Error + Send + Sync>>;
+
+pub async fn start(cx: &Cx, pwd: String) -> Res {
+    if pwd.trim_start().is_empty() {
+        cx.answer("Seems like you specify empty password").send().await?;
+        return Ok(());
+    }
+
+    match cx.update.from() {
+        Some(user) => {
+            let nickname = user.clone().username.expect("Must be user");
+            cx.answer(format!("@{} registered as bot admin.", nickname)).await?;
+        }
+        None => {
+            cx.answer("Use this command as common message").send().await?;
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn ls_groups(cx: &Cx) -> Res {
+    cx.answer(format!("List of all registered groups:")).await?;
+
+    Ok(())
+}
+
+pub async fn ls_group(cx: &Cx, group_id: String) -> Res {
+    if group_id.trim_start().is_empty() {
+        cx.answer("Seems like you forget to specify group_id").send().await?;
+        return Ok(());
+    }
+
+    cx.answer(format!("Group #{} info:", group_id)).await?;
+
+    Ok(())
+}
+
+pub async fn rm_group(cx: &Cx, group_id: String) -> Res {
+    if group_id.trim_start().is_empty() {
+        cx.answer("Seems like you forget to specify group_id").send().await?;
+        return Ok(());
+    }
+
+    cx.answer(format!("Group #{} deleted:", group_id)).await?;
+
+    Ok(())
+}
+
+pub async fn ban(cx: &Cx, username: String) -> Res {
+    if username.trim_start().is_empty() {
+        cx.answer("Seems like you forget to specify username").send().await?;
+        return Ok(());
+    }
+
+    cx.answer(format!("User {} banned:", username)).await?;
+
+    Ok(())
+}
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -2,3 +2,4 @@ pub mod command;
 
 pub mod common_command;
 pub mod group_admin_command;
+pub mod bot_admin_command;
