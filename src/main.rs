a20262ed8018a2b67a454ee79b9cdaa20bbc8dc7
migregal <devgregorymironov@gmail.com>
2021-04-02 18:25:01 +0300

    First runnable bot version added

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting dices_bot...");
+
+    let bot = Bot::from_env().auto_send();
+
+    teloxide::repl(bot, |message| async move {
+        message.answer_dice().await?;
+        respond(())
+    })
+        .await;
+}
+
