2d8e6fea443324fc7f02e284348bd28e0c370c69
maxhha <devmaxhha@gmail.com>
2021-04-07 23:08:52 +0300

    WIP mongodb

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,43 @@ pub mod utils;
 
 use command::command::answer;
 use dotenv::dotenv;
+use mongodb::{options::ClientOptions, Client};
+use tokio_compat_02::FutureExt;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    run().await;
+    // // Parse a connection string into an options struct.
+    // let mongodb_uri = String::from("mongodb://cluster-shard-00-00.bwags.mongodb.net:27017, \
+    //     mongodb://cluster-shard-00-01.bwags.mongodb.net:27017, \
+    //     mongodb://cluster-shard-00-02.bwags.mongodb.net:27017/");
+
+    // let opts = ClientOptions::
+
+    let mut client_options = ClientOptions::parse(
+        "mongodb+srv://bot:<password>@cluster.bwags.mongodb.net/develop?retryWrites=true&w=majority",
+    )
+    .compat()
+    .await
+    .unwrap();
+
+    // Manually set an option.
+    client_options.app_name = Some("tg-queue-bot".to_string());
+
+    println!("Create client");
+
+    // Get a handle to the deployment.
+    let client = Client::with_options(client_options).unwrap();
+    println!("query database");
+    let db = client.database("develop");
+    println!("query collections");
+
+    for collection_name in db.list_collection_names(None).compat().await.unwrap() {
+        println!(" - {}", collection_name);
+    }
+
+    // run().await;
 }
 
 async fn run() {
