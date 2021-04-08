03f732344ec18981cc0c34f1733575ade019e778
migregal <devmigregal@gmail.com>
2021-04-08 13:56:21 +0300

    WIP Database Facade

---
+++ src/database/database.rs
@@ -0,0 +1,25 @@
+use mongodb::{Client, options::ClientOptions};
+use tokio_compat_02::FutureExt;
+use std::env;
+use std::future::Future;
+
+// trait Database {
+//     fn new() -> Box<dyn Future<Output=Result<Self, Box<dyn std::error::Error>>>>;
+// }
+//
+// struct MongoDB(mongodb::Database);
+//
+// impl Database for MongoDB {
+//     async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+//         let mut options = ClientOptions::parse(env::var("MONGO_DSL").unwrap().as_str())
+//             .compat()
+//             .await
+//             .unwrap();
+//
+//         options.app_name = Some(env::var("BOT_NAME").unwrap());
+//
+//         let client = Client::with_options(options).unwrap();
+//
+//         Ok(MongoDB(client.database(env::var("DB_NAME").unwrap().as_str())))
+//     }
+// }
